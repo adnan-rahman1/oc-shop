3ac003486529e287b5b575d78c8ceb68e0109531
Adnan Rahman <adnanibnamotin@yahoo.com>
2019-10-13 01:34:39 -0400

    create header page component including it's styles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import HeaderPage from './components/header/header.component';
 import ShopPage from './pages/shop/shop.component';
+
 
 function App() {
   return (
     <div>
+      <HeaderPage />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
